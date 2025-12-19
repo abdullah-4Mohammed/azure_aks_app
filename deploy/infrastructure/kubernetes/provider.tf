bbb0690be6b173ff28b0d237edcb4f281e06085f
Abdullah <abdullah.4mohamed@gmail.com>
2025-12-19 17:45:23 +0000

    changing the tf providers version to more fexibles

--- deploy/infrastructure/kubernetes/provider.tf
+++ deploy/infrastructure/kubernetes/provider.tf
@@ -1,10 +1,13 @@
 terraform {
-    required_version = "1.6.0"
+    //required_version = "1.6.0"
+    required_version = ">=1.3"  # ← Change to flexible
 
     required_providers {
       azurerm = {
         source = "hashicorp/azurerm"
-        version = "3.75.0"
+        //version = "3.75.0"
+        version = "~>3.0"  # ← Change to flexible
+        
       }
     }
 }
